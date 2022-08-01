3e647c84cc43f00b3afa7a7df558f1496e0a8314
TMHuy01-08 <trinh.mai.huy@sun-asterisk.com>
2022-08-01 15:15:06 +0700

    chapter_8

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get "/signup", to: "users#new"
   resources :users, only: %i(new create show)
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
   end
 
 end
